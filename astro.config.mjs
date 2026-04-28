ea930c4d79e69fd602c811b4394d8eaaeb0e61df
James Newlin <newlin83@gmail.com>
2026-04-27 17:51:31 -0700

    links

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,6 +25,7 @@ export default defineConfig({
 				{
 					label: 'Legal Framework',
 					items: [
+						{ label: 'The Road to Ratification', slug: '/ratification/' },
 						{ label: 'The Amendment', slug: 'legal/amendment' },
 						{ label: 'The Ceremony', slug: 'legal/ceremony' },
 						{ label: 'The Contract', slug: 'legal/contract' },
